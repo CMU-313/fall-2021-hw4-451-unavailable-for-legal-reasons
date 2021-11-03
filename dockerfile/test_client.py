22c22cd77036d46354a9a74eec4f14b155377e01
prithviokade <okadeprithvi@gmail.com>
2021-11-02 23:22:35 -0400

    added more prediction test

--- dockerfile/test_client.py
+++ dockerfile/test_client.py
@@ -19,12 +19,26 @@ def test_client(client):
     assert (b'try the predict route it is great!' == r.data)
     print("Test passed!")
 
-def test_predict(client):
-    r = client.get('/predict?age=5&absences=5&health=20')
+def test_predict1(client):
+    r = client.get('/predict?age=15&absences=5&health=2')
     print("Testing prediction...", end=" ")
     assert "Internal Server Error" not in str(r.data)
-    assert int(r.prediction) == 1
     print("Test passed!")
+    print(f"Prediction received on age=18, absences=93, health=2: {r.data}")
+
+def test_predict2(client):
+    r = client.get('/predict?age=18&absences=93&health=2')
+    print("Testing prediction...", end=" ")
+    assert "Internal Server Error" not in str(r.data)
+    print("Test passed!")
+    print(f"Prediction received on age=18, absences=93, health=2: {r.data}")
+
+def test_predict3(client):
+    r = client.get('/predict?age=22&absences=1&health=5')
+    print("Testing prediction...", end=" ")
+    assert "Internal Server Error" not in str(r.data)
+    print("Test passed!")
+    print(f"Prediction received on age=22, absences=1, health=5: {r.data}")
 
 def test_empty(client):
     r = client.get('/predict?')
