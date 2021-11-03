d9059a14fa6b4c3a0f076ec30b708086e41f3d14
prithviokade <okadeprithvi@gmail.com>
2021-11-02 23:09:36 -0400

    adding applicable tests

--- dockerfile/test_client.py
+++ dockerfile/test_client.py
@@ -1,27 +1,51 @@
 import os
 import tempfile
+import requests
 
 import pytest
 
-from flaskr import create_app
-from flaskr.db import init_db
+from apps.app import app
 
+url = "http://localhost:5000/predict"
 
 @pytest.fixture
 def client():
-    db_fd, db_path = tempfile.mkstemp()
-    app = create_app({'TESTING': True, 'DATABASE': db_path})
-
     with app.test_client() as client:
-        with app.app_context():
-            init_db()
         yield client
 
-    os.close(db_fd)
-    os.unlink(db_path)
+def test_client(client):
+    r = client.get('/')
+    print("Testing client initialization...", end=" ")
+    assert (b'try the predict route it is great!' == r.data)
+    print("Test passed!")
 
-def test_empty_db(client):
-    """Start with a blank database."""
+def test_predict(client):
+    r = client.get('/predict?age=5&absences=5&health=20')
+    print("Testing prediction...", end=" ")
+    assert "Internal Server Error" not in str(r.data)
+    assert int(r.prediction) == 1
+    print("Test passed!")
 
-    rv = client.get('/')
-    assert b'No entries here so far' in rv.data
+def test_empty(client):
+    r = client.get('/predict?')
+    print("Testing empty prediction...", end=" ")
+    assert "500 Internal Server Error" in str(r.data)
+    print("Test passed!")
+
+def test_missing_one(client):
+    r = client.get('/predict?&&absences=5&health=20')
+    print("Testing incomplete prediction, missing one argument...", end=" ")
+    assert "500 Internal Server Error" in str(r.data)
+    print("Test passed!")
+
+def test_missing_multiple(client):
+    r = client.get('/predict?&&absences=5&')
+    print("Testing incomplete prediction, missing multiple arguments...", end=" ")
+    assert "500 Internal Server Error" in str(r.data)
+    print("Test passed!")
+
+def test_extra_one(client):
+    r = client.get('/predict?age=5&absences=5&health=20&test=32')
+    print("Testing addition prediction features, extra one argument...", end=" ")
+    assert "500 Internal Server Error" in str(r.data)
+    print("Test passed!")
